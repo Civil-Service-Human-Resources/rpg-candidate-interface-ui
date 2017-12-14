aa790d5127ad9530cab5db1c9af02e14d174872c
James Chapman <james@developmentdoneright.co.uk>
2017-12-14 16:09:40 +0000

    Set up initial header and footer - includes rework of global header code and css

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'RPG Candidate Interface' });
+  res.render('index', { title: 'Job Search' });
 });
 
 module.exports = router;
