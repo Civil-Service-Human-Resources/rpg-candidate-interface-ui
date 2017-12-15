0080e3ebbe0a7d90a171fc86c48aa0aae9d4ce69
James Chapman <james@developmentdoneright.co.uk>
2017-12-15 12:39:05 +0000

    Set up API call to backend and return results in plain text to browser (just returns all results for now until API endpoint for search is available)

--- routes/results.js
+++ routes/results.js
@@ -1,16 +1,29 @@
 var express = require('express');
 var router = express.Router();
 var url = require('url');
+var axios = require('axios');
+
+const VIEW_PATH = 'pages/results';
+const API_GET_RESULTS_URL = 'http://localhost:8080/vacancy'; // this needs to change once API is updated
+
+
+axios.interceptors.request.use(request => {
+  console.log(request);
+  return request;
+});
 
 /* GET results page. */
 router.get('/', function(req, res, next) {
   var params = url.parse(req.url, true);
-  res.render('pages/results', {
-    page: {
-      title: 'Search Results'
-    },
-    keyword: params.query.keyword 
-  });
+  
+  axios.get(API_GET_RESULTS_URL)
+    .then(response => 
+      res.render(VIEW_PATH, {
+        page: { title: 'Search Results' },
+        results: JSON.stringify(response.data)
+      })
+    )
+  
 });
 
 module.exports = router;
