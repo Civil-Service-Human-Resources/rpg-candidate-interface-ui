490846bcfc0e8d0313a1dbc523ecd1f4d1da34ea
James Chapman <james@developmentdoneright.co.uk>
2018-01-09 13:31:17 +0000

    Tidy up apply.js

--- routes/apply.js
+++ routes/apply.js
@@ -1,11 +1,10 @@
-var express = require('express');
-var router = express.Router();
-var url = require('url');
+const express = require('express');
+const router = express.Router();
 
 const VIEW_PATH = 'pages/apply/index';
 
 /* GET apply page. */
-router.get('/:id', function(req, res, next) {
+router.get('/:id', function(req, res) {
    
     res.render(VIEW_PATH, {
         i18n: { 
