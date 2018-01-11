54ab217e1076da72af47d62be439cd43240f8c36
James Chapman <james@developmentdoneright.co.uk>
2018-01-11 15:11:33 +0000

    Set up home search form validation feedback

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,34 @@
 const express = require('express');
 const router = express.Router();
-const {check, validationResult} = require('express-validator/check');
+const url = require('url');
+const { check, validationResult } = require('express-validator/check');
 
 /* GET home page. */
 router.get('/', [
     // validation rules
 
     check('location')
-        .isLength({min: 1}).withMessage("Location is required")
+        .isLength({min: 1}).withMessage("Location is a required field"),
 
 ], (req, res) => {
 
+    const queryString = url.parse(req.url).query;
+    const formData = url.parse(req.url, true).query;
     const validate = validationResult(req);
+    const errors = validate.mapped();
+
+    // if the form was valid, redirect to results page
+    if(queryString && validate.isEmpty()) {
+        return res.redirect(`/results?${ queryString }`)
+    }
 
     res.render('pages/index', {
         i18n: {
             ...req.translations,
-            title: req.translations.home.page.title
+            title: `${ queryString && errors ? 'Errors: ' : '' }${req.translations.home.page.title}`
         },
-        errors: validate.mapped()
+        errors: queryString ? validate.mapped() : null,
+        formData
     });
 
 });
