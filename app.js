69a06a7a3c60dcb0f0d89bfaf5d66a416d0b7c82
James Chapman <james@developmentdoneright.co.uk>
2018-01-18 15:54:04 +0000

    Custom 404 page

--- app.js
+++ app.js
@@ -124,22 +124,10 @@ app.use('/apply', apply);
 app.use('/privacy-policy', privacyPolicy);
 app.use('/cookies', cookies);
 
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-  let err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use((err, req, res) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+// catch 404 and log error
+app.use((req, res) => {
+    logger.error(`404 Not Found "${req.method} ${req.originalUrl} 404" "${req.headers['user-agent']}"`);
+    res.render('pages/errors/notFound', {});
 });
 
 module.exports = app;
