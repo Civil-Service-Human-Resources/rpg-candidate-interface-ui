67a4c60e754c4cc62ed9697cd4c81e859a9ba434
James Chapman <james@developmentdoneright.co.uk>
2018-02-01 16:53:33 +0000

    Add validation to location field on results page

--- routes/results.js
+++ routes/results.js
@@ -1,17 +1,31 @@
 const express = require('express');
 const router = express.Router();
 const url = require('url');
+const { check, validationResult } = require('express-validator/check');
 
 const {fetchVacancyList, formatResultsData, isResultsPerPageValid, RESULTS_PER_PAGE_OPTIONS, DEFAULT_RESULTS_PER_PAGE} = require('../lib/modules/vacancy');
 const {fetchDepartmentList, getDepartmentLogos} = require('../lib/modules/department');
 const {removeUrlParameter} = require('../lib/modules/url');
 
 /* GET results page. */
-router.get('/', async function (req, res) {
-    const filters = url.parse(req.url, true).query;
-    const queryString = url.parse(req.url).query;
-    const pagerUrl = `/results?${ removeUrlParameter(queryString, 'page') }`;
+router.get('/', [
 
+    check('location')
+        .trim()
+        .isLength({min: 1}).withMessage('global.messages.locationRequired')
+
+], async function (req, res) {
+    const queryString = url.parse(req.url).query;
+
+    // if someone is trying to access the page with not filters
+    // redirect to home
+    if(!queryString) {
+        return res.redirect(`/`);
+    }
+
+    const filters = url.parse(req.url, true).query;
+    const pagerUrl = `/results?${ removeUrlParameter(queryString, 'page') }`;
+    const validate = validationResult(req);
 
     const cookieRpp = req.cookies.resultsPerPage;
 
@@ -29,20 +43,20 @@ router.get('/', async function (req, res) {
     }
 
     const departments = await fetchDepartmentList();
-    const vacancies = await fetchVacancyList(filters);
+    const vacancies = validate.isEmpty() ? await fetchVacancyList(filters) : [];
 
 
     // grabbing logos directory to check existance of logo file. Temporary until future story
     // changing to CDN based file storage
     const logos = getDepartmentLogos();
     const pager = {
-        totalResults: vacancies.totalElements,
+        totalResults: vacancies.totalElements || 0,
         totalPages: vacancies.totalPages,
         currentPage: vacancies.number + 1,
         prevPage: vacancies.number,
         nextPage: vacancies.number + 2,
-        firstPage: vacancies.first,
-        lastPage: vacancies.last,
+        firstPage: vacancies.first || true,
+        lastPage: vacancies.last || true,
         url: pagerUrl
     };
 
@@ -59,7 +73,8 @@ router.get('/', async function (req, res) {
         departments: departments.content,
         returnUrl: queryString,
         pager,
-        rrp: RESULTS_PER_PAGE_OPTIONS
+        rrp: RESULTS_PER_PAGE_OPTIONS,
+        errors: !validate.isEmpty() ? validate.mapped() : null
     });
 
 });
