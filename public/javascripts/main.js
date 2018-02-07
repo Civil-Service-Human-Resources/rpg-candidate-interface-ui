b05adb30a5f85dcbf9bb761a02fd91ca34f7cce2
James Chapman <james@developmentdoneright.co.uk>
2018-02-07 09:19:23 +0000

    Tweak eslint command

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,28 +1,31 @@
 import ShowHide from '../javascripts/modules/showhide';
 
-var rppSelect = document.getElementById('rpp');
+// eslint-disable-next-line no-undef
+const rppSelect = document.getElementById('rpp');
 
-if(rppSelect) {
-    rppSelect.addEventListener('change', function (event) {
+if (rppSelect) {
+    rppSelect.addEventListener('change', (event) => {
         event.target.form.submit();
     });
 }
 
+// eslint-disable-next-line no-undef
 const showHideElements = document.getElementsByClassName('js-showhide');
-for(var i = 0; i < showHideElements.length; i++) {
-   new ShowHide({
-       el: showHideElements[i],
-       mediaQuery: 'only screen and (max-width: 640px)',
-       iconPosition: 'after'
-   });
+for (let i = 0; i < showHideElements.length; i += 1) {
+    ShowHide({
+        el: showHideElements[i],
+        mediaQuery: 'only screen and (max-width: 640px)',
+        iconPosition: 'after',
+    });
 }
 
+// eslint-disable-next-line no-undef
 const showHideFields = document.getElementsByClassName('js-showhide-field');
-for(var i = 0; i < showHideFields.length; i++) {
-    new ShowHide({
+for (let i = 0; i < showHideFields.length; i += 1) {
+    ShowHide({
         el: showHideFields[i],
         mediaQuery: 'only screen and (min-width: 0)',
         iconClassClosed: 'ion-arrow-right-b',
-        iconClassOpen: 'ion-arrow-down-b'
+        iconClassOpen: 'ion-arrow-down-b',
     });
 }
