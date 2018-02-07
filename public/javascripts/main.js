ad76841f15df14d8b89fd1cb4b019f0d8e7d2659
James Chapman <james@developmentdoneright.co.uk>
2018-02-07 09:51:49 +0000

    Refactor for loops in main.js

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -10,22 +10,18 @@ if (rppSelect) {
 }
 
 // eslint-disable-next-line no-undef
-const showHideElements = document.getElementsByClassName('js-showhide');
-for (let i = 0; i < showHideElements.length; i += 1) {
-    ShowHide({
-        el: showHideElements[i],
-        mediaQuery: 'only screen and (max-width: 640px)',
-        iconPosition: 'after',
-    });
-}
+const showHideElements = [...document.getElementsByClassName('js-showhide')];
+showHideElements.forEach(el => new ShowHide({
+    el,
+    mediaQuery: 'only screen and (max-width: 640px)',
+    iconPosition: 'after',
+}));
 
 // eslint-disable-next-line no-undef
-const showHideFields = document.getElementsByClassName('js-showhide-field');
-for (let i = 0; i < showHideFields.length; i += 1) {
-    ShowHide({
-        el: showHideFields[i],
-        mediaQuery: 'only screen and (min-width: 0)',
-        iconClassClosed: 'ion-arrow-right-b',
-        iconClassOpen: 'ion-arrow-down-b',
-    });
-}
+const showHideFields = [...document.getElementsByClassName('js-showhide-field')];
+showHideFields.forEach(el => new ShowHide({
+    el,
+    mediaQuery: 'only screen and (min-width: 0)',
+    iconClassClosed: 'ion-arrow-right-b',
+    iconClassOpen: 'ion-arrow-down-b',
+}));
