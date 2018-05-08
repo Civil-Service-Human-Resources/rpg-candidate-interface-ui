cda079854aebf0828d4d18cf34b8f841c8a9f381
James Chapman <james@developmentdoneright.co.uk>
2018-05-08 11:22:18 +0100

    CSHRCP-871 - Create account form validation (pre api request)

--- routes/account/account.js
+++ routes/account/account.js
@@ -3,6 +3,8 @@ const { check, validationResult } = require('express-validator/check');
 
 const router = express.Router();
 
+const PASSWORD_REGEX = RegExp('^(.{0,7}|[^0-9]*|[^A-Z]*|[^a-z]*|[a-zA-Z0-9]*)$');
+
 /* GET apply page. */
 router.get('/signin', (req, res) => res.render('pages/account/login', {}));
 
@@ -13,11 +15,13 @@ router.get('/activate', (req, res) => res.render('pages/account/activate', {}));
 router.post('/signin', [
     // validation rules
     check('email')
+        .trim()
         .isLength({ min: 1 })
         .withMessage('global.messages.emailRequired')
         .isEmail()
         .withMessage('global.messages.emailInvalid'),
     check('password')
+        .trim()
         .isLength({ min: 1 })
         .withMessage('global.messages.passwordRequired'),
 
@@ -34,17 +38,21 @@ router.post('/signin', [
 router.post('/create', [
     // validation rules
     check('fullname')
+        .trim()
         .isLength({ min: 1 })
         .withMessage('global.messages.fullnameRequired'),
     check('email')
+        .trim()
         .isLength({ min: 1 })
         .withMessage('global.messages.emailRequired')
         .isEmail()
         .withMessage('global.messages.emailInvalid'),
     check('password')
-        .isLength({ min: 8 })
-        .withMessage('global.messages.passwordMinLength'),
+        .trim()
+        .custom(value => !PASSWORD_REGEX.test(value))
+        .withMessage('global.messages.passwordRuleFailure'),
     check('repeatpassword')
+        .trim()
         .isLength({ min: 1 })
         .withMessage('global.messages.emailRepeatRequired')
         .custom((value, { req }) => value === req.body.password)
